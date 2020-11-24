b8481148aa73f81dfd548b3afa4a3eba25a65f2e
DaiJichen <1245487099@qq.com>
2020-11-24 00:02:13 -0500

    Proxy Pattern

---
+++ src/ProxyDemo.java
@@ -0,0 +1,39 @@
+import sun.nio.ch.Net;
+
+public class ProxyDemo {
+    public static void main(String[] args) {
+        Network proxy = new Proxy( new ConcreteNetwork() );
+        proxy.browse();
+    }
+}
+
+
+
+
+
+interface Network {
+    void browse();
+}
+
+class ConcreteNetwork implements Network {
+    @Override
+    public void browse() {
+        System.out.println("Browsing...");
+    }
+}
+
+class Proxy implements Network {
+    private Network network;
+
+    Proxy(Network network) { this.network = network; }
+
+    @Override
+    public void browse() {
+        check();
+        network.browse();
+    }
+
+    public void check() {
+        System.out.println("Checking...");
+    }
+}
